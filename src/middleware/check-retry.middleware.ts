7d2909fdf4b83b4c7afddc1f5128df8e0ff6973d
chabuuuu <haphuthinh332004@gmail.com>
2024-02-27 11:26:03 +0700

    add block user

--- src/middleware/check-retry.middleware.ts
+++ src/middleware/check-retry.middleware.ts
@@ -1,16 +1,43 @@
 import { CountRetryService } from "@/service/count-retry.service"
 import BaseError from "@/utils/baseError";
 import { HttpStatusCode } from "axios";
+import axios from "axios";
+const jwt = require('jsonwebtoken');
 const oneDayInSeconds = 60 * 60 * 24;
 const countRetryService = new CountRetryService(oneDayInSeconds);
-export const checkRetry = (maxRetry : number) => async (req: any, res: any, next: any) => {
-    const user = req.body
+const config = require('config');
+const LOAN_SERVICE = config.get('LOAN_SERVICE');
+
+async function blockUser(username: string) {
+    try {
+        const token = jwt.sign(
+            { aud: LOAN_SERVICE },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN },
+        );
+        await axios.post(`${LOAN_SERVICE}/authenticaion/block`, {username: username}, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    } catch (error) {
+        throw(error)
+    }
+    
+}
+export const checkRetry = (maxRetry: number) => async (req: any, res: any, next: any) => {
+    try {
+        const user = req.body
     if (user.username) {
         const username = user.username;
         const count = await countRetryService.getRetryCountByUsername(username);
         if (count >= maxRetry) {
-            next(new BaseError(HttpStatusCode.BadRequest, "fail","You have reached the maximum number of retries"))
+            await blockUser(username)
+            throw(new BaseError(HttpStatusCode.BadRequest, "fail", "You have reached the maximum number of retries"))
         }
     }
     next()
+    } catch (error) {
+        next(error)
+    }
 }
